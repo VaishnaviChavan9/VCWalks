06bfdc16af0103f42fc458bf0d31e9d4d3c07133
Vaishnavi Chavan <vaishnavichavan849@gmail.com>
2026-03-06 00:01:49 +0530

    initial setup

---
+++ VCWalks.API/Data/VCWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VCWalks.API.Models.Domain;
+
+namespace VCWalks.API.Data
+{
+    public class VCWalksDbContext: DbContext
+    {
+        public VCWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        
+    }
+}
